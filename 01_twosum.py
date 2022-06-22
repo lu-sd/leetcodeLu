4c53dc82fb8d0f8de514453e02c7514426ec4049
luhou2016 <luhou.me@gmail.com>
2022-06-22 19:23:20 -0400

    01 two sum

---
+++ 01_twosum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twosum(self, nums, target):
+        mapping = {}
+        for index, value in enumerate(nums):
+            diff = target-value
+            if diff not in mapping:
+                mapping[value] = index
+            else:
+                return [index, mapping[diff]]
+
+input = [2,7,11,15]
+target = 9
+A = Solution()
+result = A.twosum( input, 9 )
+print(result)
+
