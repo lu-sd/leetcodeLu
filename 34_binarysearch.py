b1bffe8fea75a3257f8468778e7efacb7a3eadcd
luhou2016 <luhou.me@gmail.com>
2022-07-01 17:31:08 -0400

    removezeros

--- 34_binarysearch.py
+++
@@ -1,8 +0,0 @@
-'''
-给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
-解法
-1、首先，在 nums 数组中二分查找 target；
-2、如果二分查找失败，则 binarySearch 返回 -1，表明 nums 中没有 target。此时，searchRange 直接返回 {-1, -1}；
-3、如果二分查找成功，则 binarySearch 返回 nums 中值为 target 的一个下标。然后，通过左右滑动指针，来找到符合题意的区间
-'''
-
